28408d9f1ae8b5483e11396fde0326e2d97760d9
RockArtist33 <680407@sandwellcollege.ac.uk>
2022-11-10 16:30:32 +0000

    Pre testing how to create UI

--- tkinter-test.py
+++ tkinter-test.py
@@ -1,22 +1,48 @@
 import tkinter as tk
+import time
 import matplotlib.pyplot as plt
 
 window = tk.Tk()
+window.geometry("600x400")
+window.columnconfigure(0,weight = 1)
+window.columnconfigure(1,weight = 1)
+entry_name = tk.StringVar()
+text1 = entry_name
 
-def plt_plot():
-    x = [1,3,5,7,9]
-    y = [3,6,9,12,18]
-    plt.scatter(x,y)
-    for index in range(0,len(x)):
-        plt.fill_between(x[index],y[index])
-    plt.show()
+def update():
+    entry_name = entry.get()
+    string_shown.config(text=entry_name)
+    print(string_shown["text"])
+    string_shown.grid(column = 1,row = 1)
+
+
+string_shown = tk.Label(
+    window,
+    text= "",
+    fg = "white",
+    bg= "black",
+    height = 2,
+    font = ("fff forward", 20)
+    )
+
+
+
+entry = tk.Entry(
+    textvariable= entry_name,
+    width = 20
+)
+
+entry.grid(column = 0,row = 1)
+
 
 Welcome = tk.Button(text="Python rocks!",
                    fg="black",
                    bg="white",
-                   width = 50,
-                   height = 20,
-                   command = plt_plot
-                   ).pack()
+                   width = 20,
+                   height = 2,
+                   command =lambda: update()
+                   )
+
+Welcome.grid(column=0, row = 2)
 
 window.mainloop()
